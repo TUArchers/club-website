eb194238826c77f091a3d269b8bfd65beb3b9ac8
James Drew <jdrew9@hotmail.co.uk>
2016-09-16 16:42:05 +0100

    Enable Google Analytics only in development mode

--- resources/views/partials/analytics.blade.php
+++ resources/views/partials/analytics.blade.php
@@ -1,4 +1,5 @@
-<!--<script>
+@if('production' == env('APP_ENV'))
+<script>
     (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
                 (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
             m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
@@ -7,4 +8,7 @@
     ga('create', 'UA-84117704-1', 'auto');
     ga('send', 'pageview');
 
-</script>-->
+</script>
+@else
+    <!--Not enabled in development mode-->
+@endif
