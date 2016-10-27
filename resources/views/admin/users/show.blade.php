635f2e556e59da3fe7582cc8a206d401bdc3e5cb
James Drew <jdrew9@hotmail.co.uk>
2016-10-27 20:31:02 +0100

    [33] Add 'empty' views to user profile

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -4,128 +4,217 @@
 
 @section('content')
     <div class="block-header">
-        <h2>USERS</h2>
+        <h2>
+            {{ strtoupper($user->name) }}<br/>
+            <small>{{ $user->role->name }}</small>
+        </h2>
+        <ul class="header-dropdown m-r-10">
+            <li class="dropdown">
+                <a href="javascript:void(0);" class="dropdown-toggle no-hover" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
+                    <i class="material-icons">more_vert</i>
+                </a>
+                <ul class="dropdown-menu pull-right">
+                    <li><a href="{{ route('admin.users.edit', $user->id) }}" class=" waves-effect waves-block">Edit User Profile</a></li>
+                </ul>
+            </li>
+        </ul>
     </div>
 
     <div class="row clearfix">
-        <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-        	<div class="card">
-                <div class="header">
-                    <h2>{{ strtoupper($user->name) }}'S PROFILE</h2>
-                    <ul class="header-dropdown m-r--5">
-                        <li class="dropdown">
-                            <a href="javascript:void(0);" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">
-                                <i class="material-icons">more_vert</i>
+        <!--Identity Column-->
+        <div class="col-xs-12 col-sm-4 col-md-3 col-lg-3">
+            <!--Image and Contact Details-->
+            <div class="card">
+                <div class="body">
+                    <img src="{{ $user->picture_url?: asset('img/user-profile-default.png') }}" alt="{{ $user->name }}'s profile picture" class="img-circle img-thumbnail center-block m-b-10">
+                    <p class="text-center font-20 m-b-40">
+                        @if('E' == $user->experience_level->id)
+                            <span class="label bg-brown">{{ $user->experience_level->name }}</span>
+                        @elseif('N' == $user->experience_level->id)
+                            <span class="label bg-orange">{{ $user->experience_level->name }}</span>
+                        @else
+                            <span class="label bg-green">{{ $user->experience_level->name }}</span>
+                        @endif
+                    </p>
+                    <h2 class="card-inside-title">Contact</h2>
+                    <ul class="list-unstyled">
+                        <li class="m-b-10">
+                            <a href="mailto: {{ $user->email }}" class="no-hover">
+                                <i class="material-icons font-18 media-middle m-r-10">email</i> {{ $user->email }}
                             </a>
-                            <ul class="dropdown-menu pull-right">
-                                <li><a href="{{ route('admin.users.edit', $user->id) }}">Edit User Profile</a></li>
-                                {{--<li><a href="#">Resend Welcome Email</a></li>--}}
-                                {{--<li><a href="#">Reset Password</a></li>--}}
-                            </ul>
+                        </li>
+                        <li class="m-b-10">
+                            <i class="material-icons font-18 media-middle m-r-10">phone</i> {{ $user->phone }}
                         </li>
                     </ul>
                 </div>
+            </div>
+        </div>
+
+        <!--Information Column-->
+        <div class="col-xs-12 col-sm-8 col-md-9 col-lg-5">
+            <!--Basic Information-->
+            <div class="card">
+                <div class="header">
+                    <h2>
+                        <i class="material-icons media-middle">person</i> Basic Information
+                    </h2>
+                </div>
                 <div class="body">
+                    <div class="p-l-30">
+                        <dl class="dl-horizontal">
+                            <dt>Full Name</dt>
+                            <dd>{{ $user->name }}</dd>
 
-                    <!--Basic Information-->
-                    <div class="row clearfix">
-                        <div class="col-xs-12 col-sm-3 col-md-3 col-lg-2">
-                            <img src="{{ $user->picture_url?: asset('img/user-profile-default.png') }}" alt="{{ $user->name }}'s profile picture" class="img-circle img-thumbnail center-block">
-                        </div>
-                        <div class="col-xs-12 col-sm-9 col-md-9 col-lg-10">
-                            <h3>
-                                {{ $user->name }} <br />
-                                <small>{{ $user->role->name }}</small>
-                            </h3>
-                            <p>
-                                @if('E' == $user->experience_level->id)
-                                    <span class="label bg-brown">{{ $user->experience_level->name }}</span>
-                                @elseif('N' == $user->experience_level->id)
-                                    <span class="label bg-orange">{{ $user->experience_level->name }}</span>
-                                @else
-                                    <span class="label bg-green">{{ $user->experience_level->name }}</span>
-                                @endif
-                            </p>
-                        </div>
-                    </div>
+                            <dt>Gender</dt>
+                            <dd>{{ $user->gender->name }}</dd>
+                            @if($user->birth_date)
 
-                    <!--Additional Information-->
-                    <div class="row clearfix">
-                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                            <h4>Contact Information</h4>
-                            <ul class="list-group">
-                                <li class="list-group-item">
-                                    <a href="mailto:{{ $user->email }}">{{ $user->email }}</a>
-                                    <i class="material-icons media-middle pull-right">email</i>
-                                </li>
-                                <li class="list-group-item">
-                                    <span>{{ $user->phone }}</span>
-                                    <i class="material-icons media-middle pull-right">phone</i>
-                                </li>
-                            </ul>
-                        </div>
-                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                            <h4>Additional Information</h4>
-                            <ul class="list-group">
-                                <li class="list-group-item">
-                                    @if($user->birth_date)
-                                        <span>{{ $user->age }} <small>years old</small></span>
-                                    @else
-                                        <span>Unknown age</span>
-                                    @endif
-                                    <i class="material-icons media-middle pull-right">cake</i>
-                                </li>
-                                <li class="list-group-item">
-                                    @if(in_array($user->gender->id, ['U', 'O']))
-                                        <span>{{ $user->gender->name }} gender</span>
-                                    @else
-                                        <span>{{ $user->gender->name }}</span>
-                                    @endif
-                                    <i class="material-icons media-middle pull-right">label_outline</i>
-                                </li>
-                                <li class="list-group-item">
-                                    <span>{{ $user->is_student? 'Student':'Non-Student' }}</span>
-                                    <i class="material-icons media-middle pull-right">school</i>
-                                </li>
-                                <li class="list-group-item">
-                                    <span>{{ $user->tusc_id?: 'Unknown' }} <small>(TUSC ID)</small></span>
-                                    <i class="material-icons media-middle pull-right">perm_identity</i>
-                                </li>
-                                <li class="list-group-item">
-                                    <span>{{ $user->agb_id?: 'Unknown' }} <small>(AGB Number)</small></span>
-                                    <i class="material-icons media-middle pull-right">card_membership</i>
-                                </li>
-                            </ul>
-                        </div>
-                    </div>
+                                <dt>Birthday</dt>
+                                <dd>{{ $user->birth_date->toFormattedDateString() }} ({{ $user->age }} <small>years old</small>)</dd>
+                            @endif
 
-                    <!--Score History-->
-                    <div class="row clearfix">
-                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                        	<h4>Recent Scores</h4>
-                            <ul class="list-group">
-                                @foreach($recent_scores as $score)
-                                <li class="list-group-item">
-                                    {{ $score->total_score }} <span class="col-grey">/ {{ $score->round->max_score }}</span> ({{ $score->round->name }} on {{ $score->shot_at->toFormattedDateString() }})
-                                    <span class="badge bg-teal">{{ $score->bow_class->name }}</span>
-                                </li>
-                                @endforeach
-                            </ul>
-                        </div>
-                        <div class="col-xs-12 col-sm-6 col-md-6 col-lg-6">
-                        	<h4>Personal Best Scores</h4>
-                            <ul class="list-group">
-                                @foreach($personal_bests as $score)
-                                    <li class="list-group-item">
-                                        {{ $score->total_score }} <span class="col-grey">/ {{ $score->round_max_score }}</span> ({{ $score->round_name }})
-                                        <span class="badge bg-teal">{{ \TuaWebsite\Domain\Records\BowClass::find($score->bow_class)->name }}</span>
-                                    </li>
-                                @endforeach
-                            </ul>
-                        </div>
+                            <dt>Student Status</dt>
+                            <dd>{{ $user->is_student? 'Student':'Non-Student' }}</dd>
+                        </dl>
                     </div>
+                </div>
+            </div>
+
+            <!--Emergency Contact-->
+            <div class="card">
+                <div class="header">
+                    <h2>
+                        <i class="material-icons media-middle">contacts</i> Emergency Contact
+                    </h2>
+                </div>
+                <div class="body">
+                    @if(!$user->emergency_contact)
+                        @include('components.empty', ['iconName' => 'warning', 'iconColour' => 'orange', 'message' => "Hmm... there doesn't seem to be an emergency contact on record for {$user->first_name }."])
+                    @else
+                    <div class="p-l-30">
+                        <dl class="dl-horizontal">
+                            <dt>Name</dt>
+                            <dd>???</dd>
+
+                            <dt>Relationship</dt>
+                            <dd>???</dd>
+
+                            <dt>Phone Number</dt>
+                            <dd>???</dd>
+
+                            <dt>Email Address</dt>
+                            <dd>???</dd>
+
+                            <dt>Address</dt>
+                            <dd>???</dd>
+                        </dl>
+                    </div>
+                    @endif
+                </div>
+            </div>
+
+            <!--Memberships-->
+            <div class="card">
+                <div class="header">
+                    <h2>
+                        <i class="material-icons media-middle">card_membership</i> Memberships
+                    </h2>
+                </div>
+                <div class="body">
+                    @if($memberships->isEmpty())
+                        @include('components.empty', ['iconName' => 'assignment', 'message' => "{$user->first_name} has no known memberships. They must be a spy."])
+                    @else
+
+                    @endif
+                    <ul class="list-unstyled list-memberships">
+                    @foreach($memberships as $membership)
+                        <li>
+                            <div class="number">{{ $membership->number }}</div>
+                            <div class="details">
+                                <strong>{{ $membership->organisation->name }}</strong>
+                                @if($membership->description) <br> {{ $membership->description }}@endif
+                                <br><span class="col-grey">{{ $membership->is_valid? 'Valid until ': 'Expired on ' }} {{ $membership->expires_at->toFormattedDateString() }}</span>
+                            </div>
+                            <div class="validity">
+                                <span class="badge {{ $membership->is_valid? 'bg-green':'bg-red' }}">{{ $membership->is_valid? 'Valid':'Expired' }}</span>
+                            </div>
+                        </li>
+                    @endforeach
+                    </ul>
+                </div>
+            </div>
+        </div>
+
+        <!--Scores Column-->
+        <div class="col-xs-12 col-sm-8 col-md-9 col-lg-4 col-sm-offset-4 col-md-offset-3 col-lg-offset-0">
+            <!--Recently Scored-->
+        	<div class="card">
+                <div class="header">
+                    <h2>
+                        <i class="material-icons media-middle">trending_up</i> Recent Scores
+                    </h2>
+                </div>
+                <div class="body">
+                    @if($recent_scores->isEmpty())
+                        @include('components.empty', ['iconName' => 'assignment', 'message' => "There are no scores on record for {$user->first_name}. Lazy."])
+                    @else
+                        <ul class="list-unstyled list-scores">
+                            @foreach($recent_scores as $score)
+                                <li>
+                                    <div class="date">{{ $score->shot_at->toFormattedDateString() }}</div>
+                                    <div class="details">
+                                        <strong>{{ $score->total_score }}</strong> <span class="col-grey">/ {{ $score->round->max_score }}</span><br>
+                                        {{ $score->round->name }}
+                                    </div>
+                                    <div class="bow-style">
+                                        <span class="badge bg-teal">{{ $score->bow_class->name }}</span>
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
+                </div>
+            </div>
+
+            <!--Personal Bests-->
+            <div class="card">
+                <div class="header">
+                    <h2>
+                        <i class="material-icons media-middle">bookmark</i> Personal Best Scores
+                    </h2>
+                </div>
+                <div class="body">
+                    @if($personal_bests->isEmpty())
+                        @include('components.empty', ['iconName' => 'assignment', 'message' => "There are no personal bests on record for {$user->first_name}. How tragic."])
+                    @else
+                        <ul class="list-unstyled list-scores">
+                            @foreach($personal_bests as $score)
+                                <li>
+                                    <div class="date">{{ $score->shot_at->toFormattedDateString() }}</div>
+                                    <div class="details">
+                                        <strong>{{ $score->total_score }}</strong> <span class="col-grey">/ {{ $score->round->max_score }}</span><br>
+                                        {{ $score->round->name }}
+                                    </div>
+                                    <div class="bow-style">
+                                        <span class="badge bg-teal">{{ $score->bow_class->name }}</span>
+                                    </div>
+                                </li>
+                            @endforeach
+                        </ul>
+                    @endif
                 </div>
             </div>
         </div>
     </div>
 @endsection
+
+@push('scripts')
+<script>
+    $(function(){
+        $('[data-toggle="tooltip"]').tooltip({
+            container: 'body'
+        });
+    });
+</script>
+@endpush
